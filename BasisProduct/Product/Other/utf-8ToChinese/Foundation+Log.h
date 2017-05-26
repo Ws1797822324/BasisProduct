ca845dea86dd6a2b98ca17a16ba3ad4add0db956
Sen wang <ws@SendeMacBook-Pro.local>
2017-05-26 11:24:35 +0800

    no message

---
+++ BasisProduct/Product/Other/utf-8ToChinese/Foundation+Log.h
@@ -0,0 +1,18 @@
+//
+//  Foundation+Log.h
+//  Product
+//
+//  Created by Sen wang on 2017/5/26.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSArray (Log)
+
+@end
+
+@interface NSDictionary (Log)
+
+@end
